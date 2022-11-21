29ebe56397cbf92415b1042723d3dbea2f20448a
Danica Yener <ny21037@googlemail.com>
2022-11-21 16:38:12 +0100

    SingleCards & AnimalCategorie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import AnimalCategories from "./components/AnimalCategories"
+
 
 import Cart from "./components/Cart";
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/animalcategories" element={<AnimalCategories />} />
 
         <Route path="/cart" element={<Cart />} />
       </Routes>
--- src/components/AnimalCategories.jsx
+++ src/components/AnimalCategories.jsx
@@ -4,62 +4,29 @@ import { Link } from "react-router-dom";
 import { Context } from '../context/ContextProvider';
 import { useParams } from "react-router-dom";
 import Cart from './Cart';
+import SingleCard from './SingleCard'
 
+const AnimalCategories = () => {
+  const [data, setData] = useState([]);
 
-const Products = () => {
-
-  const {cart, setCart} = useContext(Context);
-
-  const [categories, setCategories] = useState([]);
-
-  const { id } = useParams();
-
-  const handleClick = (item) => {
-    setCart([...cart, item])
-    console.log(cart)
-  }
 
 
   useEffect(() => {
 
-    const getCategories= async() =>{
-        const response = await fetch(`api.JSON${id}`)
-        const data = await response.json();
-        // console.log(data)
-        setCategories(data.slice(0,30))
-      };  
-      getCategories();
-  }, []);
+    const fetchData = async () => {
+      const res = await fetch(require("../data/api.JSON"));
+      const json = await res.json();
+      setData(json.animals);
+    };
+    fetchData();
+  }, [setData]);
 
+console.log(data)
   return (
 
   <div>
-    <div className="flex flex-wrap w-[80%] justify-center m-auto gap-5 mb-10 mt-10">
-
-    {
-      categories.map((item,idx) =>
-      <div key={idx} item={item} className="relative w-[16rem] mb-8 bg-gray-200 rounded-lg ">
-        <Link to={`/singleAnimal/${item.id}`}>
-        <img src={item.images} alt="" className="border-2 rounded-lg hover:opacity-75"/>
-        <div className="flex justify-between items-center ">
-          <p className='text-gray-700 py-2'>{item.title}</p>
-          <p className="font-bold text-gray-700">{item.price}€</p>
-
-        </div></Link>
-        <button onClick={() => handleClick(item)} className="absolute top-[-10px] right-[-10px] text-center bg-amber-500 rounded-xl p-1 text-white">
-        {/* <Cart aria-label="cart" >
-                <Badge className="text-white">
-                    <ShoppingCartIcon/>
-                </Badge>
-            </Cart> */}
-        </button>
-      </div>
-      )
-    }
-  </div>
-
-
+   <SingleCard data={data} />
 </div>)
 }
 
-export default Products
+export default AnimalCategories
