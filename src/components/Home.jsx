66ddef605927c6f283ee479d702b726c32ecea49
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-11-21 14:47:18 +0100

    lalala

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Form from "./Form";
 function Home() {
-const [data, setData] = useState([]);
-useEffect(() => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(require("../data/api.JSON"));
       const json = await res.json();
@@ -11,9 +12,7 @@ useEffect(() => {
     fetchData();
   }, [setData]);
 
-  return (
-   <Form data={data}></Form>
-  );
+  return <Form data={data}></Form>;
 }
 
 export default Home;
