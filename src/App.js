885ff1c5736174e99dc5542549b26f135673e026
Danica Yener <ny21037@googlemail.com>
2022-11-25 00:06:20 +0100

    singleCard, dateTime, background

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import AnimalCategories from "./components/AnimalCategories"
-
-
+import AnimalCategories from "./components/AnimalCategories";
+import SingleCard from "./components/SingleCard";
 import Cart from "./components/Cart";
+import DataTimePicker from "./components/DataTimePicker"; 
+
+
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+     
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/animalcategories" element={<AnimalCategories />} />
-
-        <Route path="/cart" element={<Cart />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/animalcategories' element={<AnimalCategories />} />
+        <Route path='/singlecard' element={<SingleCard />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/datatimepicker' element={<DataTimePicker />} />
       </Routes>
     </div>
   );
--- src/components/AnimalCategories.jsx
+++ src/components/AnimalCategories.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Context } from '../context/ContextProvider';
 import { useParams } from "react-router-dom";
 import Cart from './Cart';
 import SingleCard from './SingleCard'
